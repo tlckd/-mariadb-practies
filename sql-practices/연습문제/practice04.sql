b1f52f3beb61155240caab2506eee9e5300ec627
tlckd <tlckd8581@gmail.com>
2022-05-17 18:20:19 +0900

    연습문제 4번 4번,5번품

--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -52,26 +52,54 @@ select a.emp_no 사번, concat(first_name, last_name) as 이름 ,salary 연봉, 
 		and d.dept_name = e.dept_name
 		and b.to_date='9999-01-01'
 		and c.to_date='9999-01-01'
-		and salary > avg_salary;
+		and salary > avg_salary
+        order by 연봉 desc;
         
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
 -- 사원 -조인- dept_emp -조인- dept_manager - dept_no
 
-select a.emp_no, concat(first_name, last_name) as 이름, b.emp_no as 매니저이름, dept_name as 부서이름 
-from employees a, dept_manager b, dept_emp c , departments d 
+select a.emp_no, concat(first_name, last_name) as 이름, 매니저이름, dept_name as 부서이름 
+from employees a, dept_emp c , departments d,
+(select b.dept_no,a.emp_no,concat(first_name, last_name) as 매니저이름
+from employees a, dept_manager b 
+where a.emp_no = b.emp_no 
+and b.to_date='9999-01-01') b
 where a.emp_no = c.emp_no
 and c.dept_no = b.dept_no
 and c.dept_no = d.dept_no
-and b.to_date = '9999-01-01'
 and c.to_date = '9999-01-01';
-
-select * 
-from dept_manager;
 
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+
+select a.emp_no as 사번, concat(first_name, last_name) as 이름, title as 직책, salary as 연봉 
+from employees a, titles b, salaries c, dept_emp d 
+where a.emp_no = b.emp_no 
+and a.emp_no = c.emp_no 
+and a.emp_no = d.emp_no
+and b.to_date like '9999%'
+and c.to_date like '9999%'
+and d.to_date like '9999%'
+and d.dept_no = (select b.dept_no
+				from salaries a, dept_emp b, departments c 
+				where a.emp_no = b.emp_no 
+				and b.dept_no = c.dept_no
+				and a.to_date like '9999%'
+				and b.to_date like '9999%'
+				group by dept_name
+				having avg(salary) = (select max(a.mydept_avg)
+									from (select dept_name, avg(salary) as mydept_avg
+									from salaries a, dept_emp b, departments c 
+									where a.emp_no = b.emp_no 
+									and b.dept_no = c.dept_no
+									and a.to_date like '9999%'
+									and b.to_date like '9999%'
+									group by dept_name)a ))
+order by salary desc;
+                    
+                    
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
